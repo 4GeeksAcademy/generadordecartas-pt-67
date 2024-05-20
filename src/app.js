b3028de9c7db271905e0247e41df3e045fda4369
Sergiohc19 <161878354+Sergiohc19@users.noreply.github.com>
2024-05-20 14:25:59 +0000

    Faltaría cambiar de color los palos de corazones y diamantes a color rojo

--- src/app.js
+++ src/app.js
@@ -6,55 +6,69 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 document.addEventListener("DOMContentLoaded", function() {
   const card = document.querySelector(".card");
-  const contador = document.querySelector(".contador");
-  const boton = document.getElementById("boton");
-  const anchoInput = document.getElementById("ancho");
-  const altoInput = document.getElementById("alto");
+  const counter = document.querySelector(".counter");
+  const button = document.getElementById("button");
+  const widthInput = document.getElementById("width");
+  const heightInput = document.getElementById("height");
 
-  function cambiarCarta() {
-    card.style.width = anchoInput.value + "px";
-    card.style.height = altoInput.value + "px";
-    card.innerHTML = generateRandomCarta();
+  function widthCard() {
+    card.style.width = widthInput.value + "px";
   }
 
-  function generateRandomCarta() {
-    const palos = ["♠", "♥", "♦", "♣"];
-    const numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-    const palo = palos[Math.floor(Math.random() * palos.length)];
-    const numero = numeros[Math.floor(Math.random() * numeros.length)];
+  function heightCard() {
+    card.style.height = heightInput.value + "px";
+  }
+
+  function changeCard() {
+    card.innerHTML = generateRandomCard();
+  }
+
+  widthInput.addEventListener("input", function() {
+    widthCard();
+  });
+  heightInput.addEventListener("input", function() {
+    heightCard();
+  });
+
+  function generateRandomCard() {
+    const suits = ["♠", "♥", "♦", "♣"];
+    const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+    const suitsRandom = suits[Math.floor(Math.random() * suits.length)];
+    const numbersRandom = numbers[Math.floor(Math.random() * numbers.length)];
+
     return `
-            <span class="top-suit">${palo}</span>
-            <span class="bottom-suit">${palo}</span>
-            <span class="number">${numero}</span>
+   
+            <span class="top-suit">${suitsRandom}</span>
+            <span class="bottom-suit">${suitsRandom}</span>
+            <span class="number">${numbersRandom}</span>
         `;
   }
-
-  boton.addEventListener("click", function() {
-    cambiarCarta();
-    reiniciarContador();
+  button.addEventListener("click", function() {
+    changeCard();
+    resetCounter();
   });
 
   let segundos = 10;
   let intervalo;
 
-  function reiniciarContador() {
+  function resetCounter() {
     clearInterval(intervalo);
     segundos = 10;
-    contador.textContent = "00:10";
+    counter.textContent = "00:10";
     intervalo = setInterval(function() {
       segundos--;
       const minutos = Math.floor(segundos / 60);
       const segundosRestantes = segundos % 60;
       const segundosFormateados =
         segundosRestantes < 10 ? "0" + segundosRestantes : segundosRestantes;
-      contador.textContent = "0" + minutos + ":" + segundosFormateados;
+      counter.textContent = "0" + minutos + ":" + segundosFormateados;
       if (segundos === 0) {
         clearInterval(intervalo);
-        cambiarCarta();
-        reiniciarContador();
+        changeCard();
+        resetCounter();
       }
     }, 1000);
   }
 
-  reiniciarContador(); // Iniciar el contador al cargar la página
+  resetCounter();
 });
